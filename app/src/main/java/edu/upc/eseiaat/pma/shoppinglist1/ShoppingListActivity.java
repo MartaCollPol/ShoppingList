d3305a8ffd87d99b7a165ae84e2034bf3f1cfcaf
Marta <maly-cat@hotmail.es>
2017-10-20 17:04:42 +0100

    Project Uploaded

---
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist1/ShoppingListActivity.java
@@ -0,0 +1,100 @@
+package edu.upc.eseiaat.pma.shoppinglist1;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList<String> itemlist;
+    private ArrayAdapter<String> adapter;
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoppinglist);
+
+        //Per crear variables privades directament un cop declarades com a locals
+        //ens posem a sobre del nom d'aquesta i li donem a ctrl+alt+f i a current method.
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText)findViewById(R.id.edit_item);
+
+        itemlist= new ArrayList<>();
+        itemlist.add("Los");
+        itemlist.add("Españoles");
+        itemlist.add("Hacen");
+        itemlist.add("Cosas");
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemlist);
+        
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                addItem();
+                return false;
+            }
+        });
+
+        list.setAdapter(adapter);
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> list, View item, int pos, long id) {
+                maybeRemoveItem(pos);
+                return false;
+            }
+        });
+
+    }
+
+    private void maybeRemoveItem(final int pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.confirm);
+        String s = getResources().getString(R.string.confirm_message);
+        builder.setMessage(String.format(s,itemlist.get(pos)));
+        builder.setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                itemlist.remove(pos);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
+
+    }
+
+    private void addItem() {
+        String item_text = edit_item.getText().toString();
+        if(!item_text.isEmpty()){
+            itemlist.add(item_text);
+            adapter.notifyDataSetChanged();
+            edit_item.setText("");
+        }
+
+    }
+}
